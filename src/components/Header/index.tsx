5c2915b65e913b98924e92b0a4f69fd6baa49f9d
Charles Borges <solucao.erp@gmail.com>
2023-10-08 18:58:43 -0300

    CSS Formatting (Welcome, Header, Button)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+import './styles.css';
+
 export default function Header() {
     return (
-        <div className="container">
-            <h1>Github API</h1>
+        <div className="header-container mb30">
+            <Link to="/" className="header-link">
+                <h1 className="header-title">Github API</h1>
+            </Link>
         </div>
     );
 }
--- src/routes/Welcome/index.tsx
+++ src/routes/Welcome/index.tsx
@@ -1,11 +1,13 @@
+import './styles.css';
+
 import { Link } from "react-router-dom";
 import ButtonPrimary from "../../components/ButtonPrimary";
 
 export default function Welcome() {
     return (
-        <div className="container">
-            <h1>Desafio Github API</h1>
-            <h2>DevSuperior - Escola de programação</h2>
+        <div className="welcome-container">
+            <h1 className="welcome-title">Desafio Github API</h1>
+            <h2 className="welcome-subtitle">DevSuperior - Escola de programação</h2>
             <Link to="/profile">
                 <ButtonPrimary buttonDescription="Começar" />
             </Link>
